5d283e510330fd280d40ca672100d842ccb6e193
araujoitalo <italogustavoaraujo@gmail.com>
2022-02-17 16:46:12 -0300

    Alterando o Repositorio;

--- src/main/java/com/control/irrigation/repository/ParcelaRepository.java
+++ src/main/java/com/control/irrigation/repository/ParcelaRepository.java
@@ -1,7 +1,8 @@
 package com.control.irrigation.repository;
 
 import com.control.irrigation.model.Parcela;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ParcelaRepository extends CrudRepository<Parcela, Integer> {
+public interface ParcelaRepository extends JpaRepository<Parcela, Integer> {
 }
